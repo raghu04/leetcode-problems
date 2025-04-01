f7b2b229017f250a13cfc28b941c60dcaef76f83
Raghu S <40663555+raghu04@users.noreply.github.com>
2025-04-01 20:17:52 +0530

    fix: code refactor

--- javascript/21-merge-two-sorted-lists.js
+++ javascript/21-merge-two-sorted-lists.js
@@ -12,7 +12,7 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {
+const mergeTwoLists = function (list1, list2) {
   if (!list1) return list2;
   if (!list2) return list1;
 
