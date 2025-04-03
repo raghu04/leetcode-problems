e6973a55ea5d236e6f8d653aa1bd2a8aafeb4dc7
Raghu S <40663555+raghu04@users.noreply.github.com>
2025-04-03 19:47:24 +0530

    ispalindrome code been optimized

--- 09-palindrome-number.js
+++ 09-palindrome-number.js
@@ -6,3 +6,23 @@
 const isPalindrome = function (x) {
   return x.toString() === x.toString().split("").reverse().join("");
 };
+
+/**
+ * https://leetcode.com/problems/palindrome-number/
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome1 = function (x) {
+  const numStr = x.toString();
+  let left = 0;
+  let right = numStr.length - 1;
+
+  while (left < right) {
+    if (numStr[left] !== numStr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
