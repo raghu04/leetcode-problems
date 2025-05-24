dbbdb24c080ba5aa946104651b68cb611716f585
Raghu S <40663555+raghu04@users.noreply.github.com>
2025-05-24 07:23:48 +0530

    Update removeSuperAndSubScript.js

--- util/removeSuperAndSubScript.js
+++ util/removeSuperAndSubScript.js
@@ -1,4 +1,4 @@
-// remove sup and substrips
+// remove sup and substrips from value
 export function removeSuperAndSubScript(value) {
     return value.replace(/[\u2070-\u209F\u1D00-\u1DFF]/g, '')
 }
